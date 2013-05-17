cab01205a5abec586b65f91c4dbf8412e08eed7d
Steven Haryanto <stevenharyanto@gmail.com>
2013-05-17 12:47:47 +0000

    Import of SHARYANTO/Progress-Any-Output-LogAny-0.03 from CPAN.
    gitpan-cpan-distribution: Progress-Any-Output-LogAny
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Progress-Any-Output-LogAny-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Progress/Any/Output/LogAny.pm
+++ lib/Progress/Any/Output/LogAny.pm
@@ -4,14 +4,15 @@ use 5.010;
 use strict;
 use warnings;
 
-use Log::Any '$log';
+use Log::Any;
 
-our $VERSION = '0.02'; # VERSION
-
-# TODO: allow customizing format, show task
+our $VERSION = '0.03'; # VERSION
 
 sub new {
     my ($class, %args) = @_;
+
+    $args{template} //= "(%c/%C) %m";
+    $args{logger} //= Log::Any->get_logger();
     bless \%args, $class;
 }
 
@@ -21,15 +22,16 @@ sub update {
     my $msg = $args{message};
     return unless defined($msg);
 
-    my $meth = "debugf";
+    my $meth = "debug";
     my $level = $args{level} // "normal";
     if ($level eq 'low') {
-        $meth = "tracef";
+        $meth = "trace";
     } elsif ($level eq 'high') {
-        $meth = "infof";
+        $meth = "info";
     }
 
-    $log->$meth("(%s/%s) %s", $args{pos}, $args{target}//"?", $msg);
+    my $s = $args{indicator}->fill_template($self->{template}, %args);
+    $self->{logger}->$meth($s);
 }
 
 1;
@@ -45,28 +47,59 @@ Progress::Any::Output::LogAny - Output progress to Log::Any
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
- use Progress::Any '$progress';
- use Progress::Any::Output::LogAny;
+ use Progress::Any::Output;
 
- Progress::Any->set_output(
-     output => Progress::Any::Output::LogAny->new(
-         # options
-     ),
+ Progress::Any::Output->set_output("LogAny",
+     logger   => $log,
+     template => '(%c/%C) %m',
  );
 
- $progress->init(target=>4);
- $progress->update(message=>"Test") for 1..4;
+=head1 DESCRIPTION
 
-Will output in log:
+This output sends progress update to Log::Any. Only progress update() containing
+message will be logged, unless C<has_message_only> attribute is set to false.
+Logging is done at loglevel C<debug>, but when update C<level> is set to
+C<high>, message will be logged at loglevel C<info>. When update C<level> is set
+to C<low>, message will be logged at loglevel C<trace>.
 
- (1/4) Test
- (2/4) Test
- (3/4) Test
- (4/4) Test
+=for Pod::Coverage ^(update)$
+
+=head1 METHODS
+
+=head2 new(%args) => OBJ
+
+Instantiate. Usually called through C<<
+Progress::Any::Output->set("LogAny", %args) >>.
+
+Known arguments:
+
+=over
+
+=item * logger => OBJ
+
+Logger object to use. Defaults to C<< Log::Any->get_logger() >>.
+
+=item * template => STR (default: '(%c/%C) %m')
+
+Will be used to do C<< $progress->fill_template() >>. See L<Progress::Any> for
+supported template strings.
+
+=item * has_message_only => BOOL (default: 1)
+
+By default, when set to 1, will cause output to only log progress update()'s
+which contains message. If set to 0, will log all progress update()'s.
+
+=back
+
+=head1 SEE ALSO
+
+L<Progress::Any>
+
+L<Log::Any>
 
 =head1 AUTHOR
 
@@ -74,7 +107,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
